916445f3395d45b258d848e1a2ab29a14f535c8c
joabyjoa <joabyjoa@gmail.com>
2020-03-12 16:33:25 +0900

    Add API URL

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const MASK_URL = ''
+export const MASK_URL = 'https://m.blog.naver.com/kfdazzang/221839489769'
 
 export { default as ROUTES } from './routes'
--- src/library/fetchData.js
+++ src/library/fetchData.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-import { POST_URL } from '../constants'
+import { MASK_URL } from '../constants'
 
-export const fetchData = ({ method, url, baseURL = POST_URL, data, headers }) =>
-  Axios({ method, url, baseURL, data, headers, timeout: 500 })
+export const fetchMask = ({ method, url, baseURL = MASK_URL, data }) =>
+  Axios({ method, url, baseURL, data })
