d61d26ca930067728acf17493d1181c2f2f0acbd
joabyjoa <joabyjoa@gmail.com>
2020-03-22 15:22:52 +0900

    Change component's name

--- src/components/PageTemplate/index.jsx
+++ src/components/PageTemplate/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import { Header, Footer, Typography } from '..'
+import { ROUTES } from '../../constants'
 
-const AboutPage = ({ title, children }) => {
+const PageTemplate = ({ children }) => {
+  const { pathname } = useLocation()
+  const [{ name }] = ROUTES.filter(({ path }) => path === pathname)
   return (
     <>
       <Header />
-      <Typography variation="title">{title}</Typography>
+      <Typography variation="title">{name}</Typography>
       {children}
       <Footer />
     </>
   )
 }
 
-export default AboutPage
+export default PageTemplate
