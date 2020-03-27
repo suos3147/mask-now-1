7e62ba708b2e9a1474004980762ec0109314f5c0
joabyjoa <joabyjoa@gmail.com>
2020-03-27 23:50:48 +0900

    Change name

--- src/library/fetchData.js
+++ src/library/fetchData.js
@@ -1,7 +1,5 @@
 import Axios from 'axios'
 import { MASK_URL } from '../constants'
 
-const fetchMask = ({ method, url, baseURL = MASK_URL, data }) =>
+export const fetchMask = ({ method, url, baseURL = MASK_URL, data }) =>
   Axios({ method, url, baseURL, data })
-
-export default fetchMask
--- src/library/index.js
+++ src/library/index.js
@@ -1,4 +1,2 @@
+export { fetchMask } from './fetchData'
 export { default as usePromise } from './usePromise'
-export { default as getCenter } from './getCenter'
-
-export { default as fetchData } from './fetchData'
