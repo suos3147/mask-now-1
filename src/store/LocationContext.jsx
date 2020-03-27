02008008f9cce39936c7e49d794f94b21e9141bf
joabyjoa <joabyjoa@gmail.com>
2020-03-28 01:17:57 +0900

    Add location context

---
+++ src/store/LocationContext.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react'
+
+const initialLocation = {
+  latitude: 37.56647,
+  longitude: 126.977963,
+}
+
+const LocationContext = createContext({
+  location: initialLocation,
+  changeLocation: () => {},
+})
+
+export const LocationProvider = ({ children }) => {
+  const [location, changeLocation] = useState(initialLocation)
+
+  const value = {
+    location,
+    changeLocation: changeLocation,
+  }
+
+  return <LocationContext.Provider value={value}>{children}</LocationContext.Provider>
+}
+
+export default LocationContext
