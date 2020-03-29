8e85eed2a8925f41b85922a10301e78819bb4082
joabyjoa <joabyjoa@gmail.com>
2020-03-29 20:54:03 +0900

    아이콘 삭제 및 추가

--- src/components/Icon/index.stories.jsx
+++ src/components/Icon/index.stories.jsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import Icon from '.'
+import * as icons from '../../assets/icons'
 
 export default {
   component: Icon,
@@ -9,42 +10,19 @@ export default {
 
 export const Default = () => <Icon shape="like" />
 
-export const shapes = () => (
-  <section css={shapeContainer}>
-    <article>
-      <h4>back</h4>
-      <Icon shape="back" />
-    </article>
-    <article>
-      <h4>more</h4>
-      <Icon shape="more" />
-    </article>
-    <article>
-      <h4>like</h4>
-      <Icon shape="like" />
-    </article>
-    <article>
-      <h4>liked</h4>
-      <Icon shape="liked" />
-    </article>
-    <article>
-      <h4>comment</h4>
-      <Icon shape="comment" />
-    </article>
-    <article>
-      <h4>share</h4>
-      <Icon shape="share" />
-    </article>
-    <article>
-      <h4>bookmark</h4>
-      <Icon shape="bookmark" />
-    </article>
-    <article>
-      <h4>bookmarked</h4>
-      <Icon shape="bookmarked" />
-    </article>
-  </section>
-)
+export const shapes = () => {
+  const iconNames = Object.keys(icons)
+  return (
+    <section css={shapeContainer}>
+      {iconNames.map(name => (
+        <article>
+          <h4>{name}</h4>
+          <Icon shape={name} />
+        </article>
+      ))}
+    </section>
+  )
+}
 
 export const sizes = () => (
   <section css={container}>
