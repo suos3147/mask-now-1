47b00d2f10f258ef86d8e8f049942e3594cabb65
suos3147 <suos3147@gmail.com>
2020-03-30 22:16:56 +0900

    리스트 아이템 수정

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,31 +1,62 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx, css } from '@emotion/core'
 import PropTypes from 'prop-types'
 
 const Item = ({ data }) => {
   const { addr, name, remain_stat, stock_at } = data
+  let remain = ''
+  switch (remain_stat) {
+    case 'break':
+      remain = '판매중지'
+      break
+    case 'plenty':
+      remain = '100개 ~'
+      break
+    case 'some':
+      remain = '30개 ~ 99개'
+      break
+    case 'few':
+      remain = '2개 ~ 29개'
+      break
+    case 'empty':
+      remain = '~ 1개'
+      break
+    default:
+      remain = ''
+  }
   return (
-    <li>
+    <li css={setStyle(remain_stat)}>
+      <p>
+        <span>[{remain}]</span> 입고시간: {stock_at}
+      </p>
       <p>{name}</p>
       <p>{addr}</p>
-      <p>입고시간: {stock_at}</p>
-      {remain_stat === 'plenty' ? (
-        <span> 100개 ~ </span>
-      ) : remain_stat === 'some' ? (
-        <span> 30개 ~ 100개</span>
-      ) : remain_stat === 'few' ? (
-        <span> 2개 ~ 30개</span>
-      ) : remain_stat === 'empty' ? (
-        <span> ~ 1개</span>
-      ) : (
-        <span>판매중지</span>
-      )}
     </li>
   )
 }
 
 Item.propTypes = {
   data: PropTypes.object,
+}
+
+const setStyle = status => {
+  const backgroundColors = {
+    plenty: 'green',
+    some: 'yellow',
+    few: 'red',
+    empty: 'gray',
+    break: 'lightgray',
+  }
+  const style = css`
+    background: ${backgroundColors[status]};
+    list-style: none;
+    p {
+      span:nth-of-type(1) {
+        font-weight: bold;
+      }
+    }
+  `
+  return style
 }
 
 export default Item
