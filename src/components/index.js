c3f4848b86b9ed78c729ddd1eae5e60d6f14c334
joabyjoa <joabyjoa@gmail.com>
2020-03-31 00:24:08 +0900

    [Add] NumberPad

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,16 @@
 export { default as Button } from './Button'
 export { default as Typography } from './Typography'
 export { default as Icon } from './Icon'
+export { default as Input } from './Input'
 
 export { default as PageTemplate } from './PageTemplate'
 export { default as Header } from './Header'
 export { default as Navigation } from './Navigation'
 export { default as Footer } from './Footer'
-export { default as Input } from './Input'
+
 export { default as Overlay } from './Overlay'
 export { default as SearchBar } from './SearchBar'
 export { default as Loader } from './Loader'
 export { default as Modal } from './Modal'
 export { default as ModalWithButton } from './Modal/ModalWithButton'
+export { default as NumberPad } from './NumberPad'
