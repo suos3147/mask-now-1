69d78dd5dcf471820638b96f92f57370d14e5522
pathas <pathas@naver.com>
2020-03-30 17:42:23 +0900

    Add Modal Component, Coockie Function

--- src/library/index.js
+++ src/library/index.js
@@ -2,3 +2,5 @@ export { fetchMask } from './fetchData'
 export { default as usePromise } from './usePromise'
 
 export { default as useCurrentLocation } from './useCurrentLocation'
+
+export { getCookie, createCookie } from './useCookie'
