9c569ce7d39360b380a1c6545440d0314f829e71
joabyjoa <joabyjoa@gmail.com>
2020-03-31 00:38:29 +0900

    [Add] Page info

--- src/components/PageTemplate/index.jsx
+++ src/components/PageTemplate/index.jsx
@@ -1,13 +1,18 @@
 /** @jsx jsx */
 import { Fragment } from 'react'
 import { jsx, css } from '@emotion/core'
-import { Header, Footer } from '..'
+import { Header, Footer, Typography } from '..'
 
-const PageTemplate = ({ children }) => {
+const PageTemplate = ({ children, info }) => {
   return (
     <Fragment>
       <Header />
-      <main css={mainStyle}>{children}</main>
+      <main css={mainStyle}>
+        <Typography variation="callout" color="secondary" className="info">
+          {info}
+        </Typography>
+        {children}
+      </main>
       <Footer color="primary" />
     </Fragment>
   )
@@ -21,6 +26,11 @@ const mainStyle = css`
   height: 100vh;
   background: #fff;
   z-index: 1;
+  .info {
+    margin: 2rem auto;
+    width: 80vw;
+    text-align: center;
+  }
 `
 
 export default PageTemplate
