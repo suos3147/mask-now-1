dede0a7b2aaac7f2a3d4f49673a4fa546bec06c7
suos3147 <suos3147@gmail.com>
2020-03-31 18:05:07 +0900

    Loader 컴포넌트 사용

--- src/containers/ListContainer.jsx
+++ src/containers/ListContainer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { fetchMask, usePromise } from '../library'
-import { List } from '../components'
+import { List, Loader } from '../components'
 
 const ListContainer = () => {
   const [loading, response, error] = usePromise(() => {
     return fetchMask({ method: 'GET', url: '/storesByGeo/json' })
   }, [])
 
-  if (loading) return <p>Loading...</p>
+  if (loading) return <Loader></Loader>
   if (error) return <p>What happen?😧</p>
   if (!response) return null
 
