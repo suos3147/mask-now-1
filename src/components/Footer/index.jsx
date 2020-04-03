08a09f8c6201f949bd22687075e8fa6c86f3e7dc
pathas <pathas@naver.com>
2020-04-04 00:50:44 +0900

    Fix footer link bug

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -42,9 +42,9 @@ const Footer = ({ color }) => {
 
 const boxShadow = css`
   width: 100%;
-  height: 54px;
+  height: 3px;
+  bottom: 51px;
   position: absolute;
-  bottom: 0;
   z-index: 2;
   box-shadow: 0px -2px 2px 0px rgba(0, 0, 0, 0.32), 0px -3px 4px 1px rgba(30, 144, 255, 0.45);
   transition: 0.2s ease-out;
