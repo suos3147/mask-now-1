1d8532524489e86c919b967e12295fcf485bb770
pathas <pathas@naver.com>
2020-04-08 23:23:34 +0900

    Change object to array in DayContainer

--- src/containers/DayContainer.jsx
+++ src/containers/DayContainer.jsx
@@ -5,13 +5,7 @@ import { ModalWithDay } from '.'
 const DayContainer = () => {
   const [modal, setModal] = useState(false)
   const [day, setDay] = useState(0)
-  const days = {
-    1: '월요일',
-    2: '화요일',
-    3: '수요일',
-    4: '목요일',
-    0: '금요일',
-  }
+  const days = ['금요일', '월요일', '화요일', '수요일', '금요일']
   const popupModal = number => {
     setDay(number)
     setModal(true)
